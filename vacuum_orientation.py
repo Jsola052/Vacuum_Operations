522694b053a03ba249c15144009a5afca621b350
Jsola <jsola052@fiu.edu>
2024-04-11 13:53:54 -0400

    Updated vacuum script

--- vacuum_orientation.py
+++
@@ -1,9 +0,0 @@
-path = [[0.100, 0.200, 0.100],
-        [0.100, 0.400, 0.100],
-        [0.150, 0.400, 0.100],
-        [0.300, 0.400, 0.100],
-        [0.300, 0.300, 0.100]]
-for x in path:
-    print(x)
-    temp_vector = path[path.index(x) + 1] - x
-    print ("temp_vector = " + temp_vector)
